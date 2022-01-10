64d2e119c5ae063399a56fdb193908e2b76bf6f2
noxiossy <cari0us@outlook.com>
2022-01-10 21:57:16 +0700

    Update _stl_extensions.h

--- src/xrCore/_stl_extensions.h
+++ src/xrCore/_stl_extensions.h
@@ -207,11 +207,46 @@ protected:
     _C c;
 };
 
-template <typename T, typename allocator = xalloc<T> > class xr_list : public std::list < T, allocator > { public: u32 size() const { return (u32)__super::size(); } };
-template <typename K, class P = std::less<K>, typename allocator = xalloc<K> > class xr_set : public std::set < K, P, allocator > { public: u32 size() const { return (u32)__super::size(); } };
-template <typename K, class P = std::less<K>, typename allocator = xalloc<K> > class xr_multiset : public std::multiset < K, P, allocator > { public: u32 size() const { return (u32)__super::size(); } };
-template <typename K, class V, class P = std::less<K>, typename allocator = xalloc<std::pair<K, V> > > class xr_map : public std::map < K, V, P, allocator > { public: u32 size() const { return (u32)__super::size(); } };
-template <typename K, class V, class P = std::less<K>, typename allocator = xalloc<std::pair<K, V> > > class xr_multimap : public std::multimap < K, V, P, allocator > { public: u32 size() const { return (u32)__super::size(); } };
+#include <unordered_map>
+
+template <typename K, class V, class Hasher = std::hash<K>, class Traits = std::equal_to<K>,
+          typename allocator = xalloc<std::pair<const K, V>>>
+using xr_unordered_map = std::unordered_map<K, V, Hasher, Traits, allocator>;
+
+template <typename T, typename allocator = xalloc<T>>
+class xr_list : public std::list<T, allocator>
+{
+public:
+	u32 size() const { return (u32)__super::size(); }
+};
+
+template <typename K, class P = std::less<K>, typename allocator = xalloc<K>>
+class xr_set : public std::set<K, P, allocator>
+{
+public:
+	u32 size() const { return (u32)__super::size(); }
+};
+
+template <typename K, class P = std::less<K>, typename allocator = xalloc<K>>
+class xr_multiset : public std::multiset<K, P, allocator>
+{
+public:
+	u32 size() const { return (u32)__super::size(); }
+};
+
+template <typename K, class V, class P = std::less<K>, typename allocator = xalloc<std::pair<K, V>>>
+class xr_map : public std::map<K, V, P, allocator>
+{
+public:
+	u32 size() const { return (u32)__super::size(); }
+};
+
+template <typename K, class V, class P = std::less<K>, typename allocator = xalloc<std::pair<K, V>>>
+class xr_multimap : public std::multimap<K, V, P, allocator>
+{
+public:
+	u32 size() const { return (u32)__super::size(); }
+};
 
 #ifdef STLPORT
 template <typename V, class _HashFcn = std::hash<V>, class _EqualKey = std::equal_to<V>, typename allocator = xalloc<V> > class xr_hash_set : public std::hash_set < V, _HashFcn, _EqualKey, allocator > { public: u32 size() const { return (u32)__super::size(); } };
@@ -220,7 +255,13 @@ template <typename V, class _HashFcn = std::hash<V>, class _EqualKey = std::equa
 template <typename K, class V, class _HashFcn = std::hash<K>, class _EqualKey = std::equal_to<K>, typename allocator = xalloc<std::pair<K, V> > > class xr_hash_map : public std::hash_map < K, V, _HashFcn, _EqualKey, allocator > { public: u32 size() const { return (u32)__super::size(); } };
 template <typename K, class V, class _HashFcn = std::hash<K>, class _EqualKey = std::equal_to<K>, typename allocator = xalloc<std::pair<K, V> > > class xr_hash_multimap : public std::hash_multimap < K, V, _HashFcn, _EqualKey, allocator > { public: u32 size() const { return (u32)__super::size(); } };
 #else
-template <typename K, class V, class _Traits = stdext::hash_compare<K, std::less<K> >, typename allocator = xalloc<std::pair<K, V> > > class xr_hash_map : public stdext::hash_map < K, V, _Traits, allocator > { public: u32 size() const { return (u32)__super::size(); } };
+template <typename K, class V, class _Traits = stdext::hash_compare<K, std::less<K>>, typename allocator = xalloc<std::
+	          pair<K, V>>>
+class xr_hash_map : public stdext::hash_map<K, V, _Traits, allocator>
+{
+public:
+	u32 size() const { return (u32)__super::size(); }
+};
 #endif // #ifdef STLPORT
 
 #endif
